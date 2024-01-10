61bd9035fec2662842ecdd3226f2991e508796ad
alexandrea parks <alparks@idk.com>
2024-01-10 17:11:21 -0600

    Lab 1 Completed

--- hello_birthday_month.py
+++ hello_birthday_month.py
@@ -2,28 +2,74 @@
 Alexandrea Parks
 hello_birthday_month.py
 1/10/2024
-Description:
+This program asks the user for their name and birth month and tells the user a greeting with their name, the length of
+their name, and if it's currently their birth month or not.
 """
 
-# import datetime
+# import datetime module
 from datetime import datetime
 
-today_datetime = datetime.now()
 
-current_month_text = today_datetime.strftime("%B")
-print(f"Current month: {current_month_text}")
+# main function to call other functions
+def main():
+    # call inputs function to get name and birthday month
+    name, birthday_month = inputs()
+    # call processing function with name as argument to get count of letters in name and the current month
+    letters_in_name, current_month_text = processing(name)
+    # call outputs function with all necessary arguments to output data to user
+    outputs(name, birthday_month, letters_in_name, current_month_text)
 
 
-name = input("Please enter your name: ")
-birthday_month = input("Please enter the month you were born: ")
+# function to get user input for name and birthday month
+def inputs():
+    # get user input
+    name = input("Please enter your name: ")
+    birthday_month = input("Please enter the month you were born: ")
+    return name, birthday_month  # return to main function
 
-print(f"Hello {name}!")
 
-letters_in_name = len(name)
+# function to get current month and length of name
+def processing(name):
+    # call method to get current month
+    current_month_text = get_current_month()
 
-print(f"Your name {name} has {letters_in_name} letters.")
+    # use len() function to get length of name
+    letters_in_name = len(name)
 
-if birthday_month.lower() == current_month_text.lower():
-    print("Happy birthday month!")
-else:
-    print("Your birthday is not this month!")
+    return letters_in_name, current_month_text  # return to main function
+
+
+# function to get the current month as a string
+def get_current_month():
+    # create a datetime object with current date and time
+    today_datetime = datetime.now()
+
+    # use .strftime() method to convert date object to string with directive for month
+    current_month_text = today_datetime.strftime("%B")  # %B is the directive for month
+
+    return current_month_text  # return to main function
+
+
+# function to output data
+def outputs(name, birthday_month, letters_in_name, current_month_text):
+    # print greeting with name
+    print(f"Hello {name}!")
+
+    # print length of name
+    print(f"Your name {name} has {letters_in_name} letters.")
+
+    # use if else logic to determine if it's the user's birth month or not
+    if birthday_month.lower() == current_month_text.lower():  # change both to lower so it's case-insensitive
+        print("Happy birthday month!")  # print if it's their birth month
+    else:
+        print("Your birthday is not this month!")  # print if it's NOT their birth month
+
+
+main()  # call main function to start program
+
+
+
+
+
+
+
--- list_of_classes.py
+++ list_of_classes.py
@@ -1,19 +1,44 @@
+"""
+Alexandrea Parks
+list_of_classes.py
+1/10/2024
+This program asks the user how many classes they are taking and the name of each of their classes. The class names are
+stored in a list and then printed with each class name on a separate line.
+"""
 
 
-# ask how many classes
-num_of_classes = int(input("Enter the number of classes you are taking: "))
-class_list = [] # create empty list to store class names
+# main function to call other functions
+def main():
+    # call inputs function to get the list of classes
+    class_list = inputs()
 
-# for loop to ask for name of each class
-for c in range(0, num_of_classes):
-    class_name = input(f"Enter the name of class number {c + 1}: ")
-    class_list.append(class_name)
+    # call outputs with the class list as an argument to output the list of classes
+    outputs(class_list)
 
-# for loop to print each class in the list
-print("Your class names are: ")
-for c in range(len(class_list)):
-    print(class_list[c])
 
+# function to get user input
+def inputs():
+    # ask how many classes user is taking
+    num_of_classes = int(input("Enter the number of classes you are taking: "))
+    class_list = []  # create empty list to store class names
+
+    # for loop to ask for name of each class
+    for c in range(0, num_of_classes):  # start at 0, count up to but don't include the num_of_classes value
+        class_name = input(f"Enter the name of class #{c + 1}: ") # add 1 so counter starts at 1 instead of 0
+        class_list.append(class_name)
+
+    return class_list  # return to main function
+
+
+# function to output data
+def outputs(class_list):
+    # for loop to print each class in the list
+    print("Your class names are: ")
+    for c in range(len(class_list)):  # 0 up to but not including length of class_list value
+        print(class_list[c])  # use index number (c) to get items from list and print them
+
+
+main()  # call main function to start program
 
 
 
